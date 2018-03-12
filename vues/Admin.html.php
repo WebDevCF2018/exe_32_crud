38689189f5aec6ba045ef116569f2d24d9e2a4b3
michaeljpitz <michaeljpitz@gmail.com>
2018-03-12 15:27:04 +0100

    button update delete

--- vues/Admin.html.php
+++ vues/Admin.html.php
@@ -23,9 +23,10 @@
         foreach ($articles AS $item) {
             ?>
             <h3><a href="?article=<?= $item['idarti'] ?>"><?= $item['titre'] ?></a></h3>
-            <!-- ici le foreach pour les categ -->
             <p><?= $item['texte'] ?> ... <a href="?article=<?= $item['idarti'] ?>">Lire la suite</a></p>
-            <p><?= $item['publie'] ?></p>
+            <p><?= $item['publie'] ?>
+               |  <img width="16px" height="16px" src="vues/img/update.png" alt="Modifier l'article" onclick="document.location='?admin&modif=<?= $item['idarti'] ?>';" /> | <img width="16px" height="16px" src="vues/img/delete.png" alt="Supprimer l'article" onclick="document.location='?admin&sup=<?= $item['idarti'] ?>';" />
+            </p>
             <hr>
             <?php
         }
