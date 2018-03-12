b1c4394fddd15ac82534d6b4a78d4b2ab93a165b
michaeljpitz <michaeljpitz@gmail.com>
2018-03-12 16:02:33 +0100

    form insert ok

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -6,8 +6,17 @@
 // chargement des modeles
 require_once "modeles/ArticleModele.php";
 
+// si on a cliqué sur ajouter
+if (isset($_GET['ajout'])) {
+
+    // on appel la vue contenant le formulaire
+    require_once "vues/FormInsert.html.php";
+
+} else {
+
 // on récupère les résumés d'articles depuis arti
-$articles = listeArtiAccueil($mysqli);
+    $articles = listeArtiAccueil($mysqli);
 
 // on prend la vue
-require_once "vues/Admin.html.php";
+    require_once "vues/Admin.html.php";
+}
